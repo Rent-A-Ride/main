656001097893cf2463950830d4a3643150ada379
Buddhiyp99 <2020cs223@stu.ucsc.cmb.ac.lk>
2023-02-08 16:02:25 +0530

    vehilce owner's edit profile, payments UIs added

---
+++ views/VehicleOwner/vehicleOwnerPayments.php
@@ -0,0 +1,114 @@
+<?php
+?>
+<h2 class="name">Manage Payments</h2>
+
+<div class="pay-body">
+
+    <div class="pay-details">
+
+
+        <div class="earnings">
+            <div class="totalearnings">
+                <h5 class="tot-ear-name">Total Earnings</h5>
+            </div>
+            <div class="totalearningsvalue">
+                <h5 class="tot-ear-value">24,000</h5>
+            </div>
+        </div>
+
+
+        <div class="holds">
+            <div class="onhold">
+                <h5 class="on-hold-name">On Hold</h5>
+            </div>
+            <div class="onholdvalue">
+                <h5 class="on-hold-value">4,000</h5>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="completed-journeys">
+        <div class="journeys">
+            <h3 class="journey-box-name">Completed Journeys</h3>
+            <div class="card">
+
+                <div class="image">
+                    <img src="/assets/img/vehicleOwner_img/car.jpeg">
+                </div>
+                <div class="title">
+                    <h1 class="plate-no">
+                        WP-KX-8710</h1>
+                </div>
+                <div class="des">
+                    <p> Add Journey Details...</p>
+                    <!-- <button>Read More...</button> -->
+                </div>
+            </div>
+
+            <div class="card">
+
+                <div class="image">
+                    <img src="/assets/img/vehicleOwner_img/car.jpeg">
+                </div>
+                <div class="title">
+                    <h1 class="plate-no">
+                        WP-KX-8710</h1>
+                </div>
+                <div class="des">
+                    <p> Add Journey Details...</p>
+                    <!-- <button>Read More...</button> -->
+                </div>
+            </div>
+
+            <div class="card">
+
+                <div class="image">
+                    <img src="/assets/img/vehicleOwner_img/car.jpeg">
+                </div>
+                <div class="title">
+                    <h1 class="plate-no">
+                        WP-KX-8710</h1>
+                </div>
+                <div class="des">
+                    <p> Add Journey Details...</p>
+                    <!-- <button>Read More...</button> -->
+                </div>
+            </div>
+
+
+<!--            <div class="card">-->
+<!---->
+<!--                <div class="image">-->
+<!--                    <img src="car.jpeg">-->
+<!--                </div>-->
+<!--                <div class="title">-->
+<!--                    <h1>-->
+<!--                        WP-KX-8710</h1>-->
+<!--                </div>-->
+<!--                <div class="des">-->
+<!--                    <p> Add Journey Details...</p>-->
+<!--                    <button>Read More...</button> -->
+<!--                </div>-->
+<!--            </div>-->
+
+        </div>
+        <div>
+
+        </div>
+
+    </div>
+
+    <div class="PAY-BUTTONS">
+        <button class="report-button">Generate Report</button>
+        <button class="veh-rates">Edit Vehicle Rates</button>
+    </div>
+
+
+
+
+
+
+
+</div>
