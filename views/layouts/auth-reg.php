4325f639f91e343406c251a4c8848b4956ad8f31
iamhasantha <2020cs090@stu.ucsc.cmb.ac.lk>
2023-04-13 14:40:52 +0530

    Registration for customer fixed

---
+++ views/layouts/auth-reg.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+
+    <title>Rent A Ride</title>
+
+
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <!-- Nav CSS -->
+    <link rel="stylesheet" href="/assets/css/login.css">
+
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="container-icon">
+            <a href=""><img class="logo" src="/assets/img/logo.png" alt="Rent a Ride Logo"></a>
+        </div>
+<!--        <ul class="nav-list" id="nav-list">-->
+<!--            <li class="list-item 1"><a href="/login">Sign in</a></li>-->
+<!--            <li class="list-item 2"><a href="/register">Register</a></li>-->
+<!--        </ul>-->
+<!--        <div id="toggle-btn" class="menu-container" onclick="myFunction(this)">-->
+<!--            <div class="bar1"></div>-->
+<!--            <div class="bar2"></div>-->
+<!--            <div class="bar3"></div>-->
+<!--        </div>-->
+    </nav>
+
+
+    {{content}}
+
+</body>
+
+</html>
